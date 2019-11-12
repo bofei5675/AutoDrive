958c3ed67d74887374148c8170fdf9cba8f46999
bofei5675 <8196385@qq.com>
2019-11-12 10:34:12 -0500

    baseline code

---
+++ make_prediction.py
@@ -0,0 +1,39 @@
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from tqdm import tqdm#_notebook as tqdm
+import os
+import torch
+from torch.utils.data import Dataset, DataLoader
+from utils import coords2str, CarDataset, MyUNet, double_conv, up, extract_coords
+
+print('Loading ...')
+PATH = './data/'
+test = pd.read_csv(PATH + 'sample_submission.csv')
+test_images_dir = PATH + 'test_images/{}.jpg'
+df_test = test
+test_dataset = CarDataset(df_test, test_images_dir, training=False)
+load_model = '/scratch/bz1030/auto_drive/saved/model_3.pth'
+predictions = []
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if torch.cuda.is_available():
+    model = torch.load(load_model)
+else:
+    model = torch.load(load_model, map_location='cpu')
+
+
+test_loader = DataLoader(dataset=test_dataset, batch_size=4, shuffle=False, num_workers=4)
+
+model.eval()
+print('Start Evaluation ...')
+for img, _, _ in tqdm(test_loader):
+    with torch.no_grad():
+        output = model(img.to(device))
+    output = output.data.cpu().numpy()
+    for out in output:
+        coords = extract_coords(out)
+        s = coords2str(coords)
+        predictions.append(s)
+
+test = pd.read_csv(PATH + 'sample_submission.csv')
+test['PredictionString'] = predictions
+test.to_csv('predictions.csv', index=False)
+test.head()
