8f492ae2a30396df776ed5899ef04964b5a75392
bofei5675 <8196385@qq.com>
2020-01-06 23:14:03 -0500

    attn mechanism

--- scripts/run_experiments_stack_4.s
+++ scripts/run_experiments_stack_4.s
@@ -4,8 +4,10 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --time=24:30:00
 #SBATCH --mem=16GB
-#SBATCH --gres=gpu:k80:1
+#SBATCH --gres=gpu:p40:2
 
 source activate capstone
 cd ../
-python main.py -m HG2 -sd run2/ -nc 8 -ns 4 -nf 256 -lf FL -s 5 -db no -tp 0.2 -pt yes -vs 0.2 -g 1
+python main.py -m HG2 -sd run2/ -nc 8\
+ -ns 8 -nf 256 -lf FL -s 1\
+ -db no -tp 0.2 -pt yes -vs 0.2 -g 10 -uc yes
